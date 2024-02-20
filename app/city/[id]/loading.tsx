5301d2f293663b058afa91afb7fcf26f7156db74
Gustavo Fonseca <gus.fonnseca@gmail.com>
2024-02-20 11:14:18 -0300

    fix: adjust skeleton widths that were breaking the layout

--- app/city/[id]/loading.tsx
+++ app/city/[id]/loading.tsx
@@ -9,20 +9,20 @@ export default function Loading() {
 			<div className="flex flex-col gap-5 mx-auto max-w-5xl h-[50vh]">
 				<BackButton />
 				<div className="flex flex-col flex-1 justify-center gap-2">
-					<Skeleton className="h-8 w-[500px] my-5" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px]" />
-					<Skeleton className="h-4 w-[1000px] mb-5" />
+					<Skeleton className="h-8 w-[50%] my-5" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%]" />
+					<Skeleton className="h-4 w-[80%] mb-5" />
 					<Skeleton className="h-[600px] w-full" />
 				</div>
 			</div>
